3b2fadd268204782be9b8520fbfd34f963f7f46c
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2025-08-18 19:06:08 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+# Telegram credentials
+API_ID = int(os.getenv("apiid", 12345))
+API_HASH = os.getenv("apihash", "your_api_hash")
+BOT_TOKEN = os.getenv("tk", "your_bot_token")
+CHAT_ID = int(os.getenv("chat", "-100123456789"))
+
+# File limits
+MAX_SIZE = 1900 * 1024 * 1024  # 1.9GB
