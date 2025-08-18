9aef58d89acdcfe36d1e9cffac4b002687d764d2
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2025-08-18 21:03:52 +0530

    Update uploader.py

--- uploader.py
+++ uploader.py
@@ -2,6 +2,10 @@ import math
 from time import time
 from config import CHAT_ID
 
+last_t={
+    text:"",
+};
+
 async def upload_with_progress(client, filename, thumb, duration, progress_msg, file_id):
     start_time = time()
 
@@ -11,13 +15,16 @@ async def upload_with_progress(client, filename, thumb, duration, progress_msg, 
         eta = (total - current) / speed if speed > 0 else 0
         if int(time() - start_time) % 5 == 0:  # update every 5s
             try:
-                await progress_msg.edit_text(
-                    f"⬆️ **Uploading** `{filename}`\n"
-                    f"Progress: {percent:.2f}%\n"
-                    f"Uploaded: {current/1024/1024:.2f}MB / {total/1024/1024:.2f}MB\n"
-                    f"Speed: {speed/1024/1024:.2f} MB/s\n"
-                    f"ETA: {math.ceil(eta)}s"
+                tt = (
+                     f"⬆️ **Uploading** `{filename}`\n"
+                     f"Progress: {percent:.2f}%\n"
+                     f"Uploaded: {current/1024/1024:.2f}MB / {total/1024/1024:.2f}MB\n"
+                     f"Speed: {speed/1024/1024:.2f} MB/s\n"
+                     f"ETA: {math.ceil(eta)}s"
                 )
+                if tt != last_t["text"]:
+                    await progress_msg.edit_text(tt)
+                    last_t["text"] = tt
             except:
                 pass
 
