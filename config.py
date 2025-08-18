41d158eaf1953869d2956d9b4e23a9e0ffa82313
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2025-08-18 21:36:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,9 @@ import os
 API_ID = int(os.getenv("apiid", 12345))
 API_HASH = os.getenv("apihash", "your_api_hash")
 BOT_TOKEN = os.getenv("tk", "your_bot_token")
-CHAT_ID = int(os.getenv("chat", "-100123456789"))
+CHAT_ID = int(os.getenv("chat"))
+
+
 
 # File limits
 MAX_SIZE = 1900 * 1024 * 1024  # 1.9GB
